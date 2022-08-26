2e17fd85d18b12a843c00a8f0dfe7205103b02ba
AI-Innovator <wuedison662@gmail.com>
2022-08-26 12:50:43 -0400

    Include contact address

--- Frontend/src/components/Footer.vue
+++ Frontend/src/components/Footer.vue
@@ -2,16 +2,16 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6">
     <div class="flex justify-end">
       <div class="">
-        <a href="#" class="text-green-700 mr-2">
+        <a href="https://prenes.slack.com/team/U02LTKCF4TW" class="text-green-700 mr-2">
           <i id="social-fb" class="fab fa-slack text-3xl social"></i>
         </a>
-        <a href="#" class="text-green-700 mr-2">
+        <a href="https://discordapp.com/users/prenes#5716" class="text-green-700 mr-2">
             <i id="social-fb" class="fab fa-discord text-3xl social"></i>
         </a>
-        <a href="#" class="text-green-700 mr-2" >
+        <a href="skype:live:.cid.12025921c037735c?chat" class="text-green-700 mr-2" >
             <i id="social-tw" class="fab fa-skype text-3xl social"></i>
         </a>
-        <a href="#" class="text-green-700 mr-2">
+        <a href="mailto:prenes.contact@gmail.com" class="text-green-700 mr-2">
             <i id="social-em" class="fa fa-envelope-square text-3xl social"></i>
         </a>
       </div>
