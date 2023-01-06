cac6eb2019943e2175fa578524f29c74c9cf3312
AI-Innovator <prenes.contact@gmail.com>
2023-01-06 09:48:35 -0500

    Add more products

--- Frontend/src/components/Product.vue
+++ Frontend/src/components/Product.vue
@@ -10,7 +10,7 @@
           Face Detection, Liveness Detection, Face Alignment, Landmark Extraction, Age and Gender Estimation,
           Expression Detection, Face Feature Extraction
         </p>
-<!--        <a href="https://github.com/AI-Innovator/Face-SDK-Javascript" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-tw" class="fab fa-github text-xl social pr-2"></i>Try it</a>-->
+        <a href="https://github.com/AI-Innovator/Face-Recognition-SDK-Javascript" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-github" class="fab fa-github text-xl social pr-2"></i>Try it</a>
       </div>
     </div>
     <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
@@ -23,7 +23,7 @@
           This application will automatically scan through your selected folder and separate
           your shots into folders using your sky shots or white shots.
         </p>
-<!--        <a href="https://github.com/AI-Innovator/Shot-Separator" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-tw" class="fab fa-github text-xl social pr-2"></i>Try it</a>-->
+        <a href="https://github.com/AI-Innovator/Shot-Separator" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-github" class="fab fa-github text-xl social pr-2"></i>Try it</a>
       </div>
     </div>
     <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
@@ -35,7 +35,7 @@
         <p class="text-gray-700 text-base mb-4">
           Python Package to denoise degraded document image in OCR.
         </p>
-<!--        <a href="https://github.com/AI-Innovator/CleanOCR" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-tw" class="fab fa-github text-xl social pr-2"></i>Try it</a>-->
+        <a href="https://github.com/AI-Innovator/CleanOCR" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-github" class="fab fa-github text-xl social pr-2"></i>Try it</a>
       </div>
     </div>
     <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
@@ -47,7 +47,7 @@
         <p class="text-gray-700 text-base mb-4">
           It implements object detection and classification, tracking algorithms. (Vehicle Detection, Pedestrian Detection, and Animal Detection, Face Detection and recognition)
         </p>
-<!--        <router-link to="/auth/signin" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400">Try it</router-link>-->
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
       </div>
     </div>
   </div>
@@ -61,7 +61,7 @@
         <p class="text-gray-700 text-base mb-4">
           It uses face detection and recognition solution to make camera surveillance system.
         </p>
-<!--        <router-link to="/auth/signin" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400">Try it</router-link>-->
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
       </div>
     </div>
     <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
@@ -73,7 +73,7 @@
         <p class="text-gray-700 text-base mb-4">
           It includes mobile and web application using face recognition to manage the employee and customers.
         </p>
-<!--        <router-link to="/auth/signin" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400">Try it</router-link>-->
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
       </div>
     </div>
     <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
@@ -85,7 +85,7 @@
         <p class="text-gray-700 text-base mb-4">
           This app remove background from the image and cartoonize the object.
         </p>
-<!--        <a href="http://34.136.19.247/" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400">Try it</a>-->
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
       </div>
     </div>
     <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
@@ -97,7 +97,57 @@
         <p class="text-gray-700 text-base mb-4">
           This tool makes background check process for employers and identification for KYC documents.
         </p>
-<!--        <router-link to="/auth/signin" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400" hidden>Try it</router-link>-->
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-center">
+    <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
+      <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+        <img class="rounded-t-lg" src="../assets/images/card12.jpg" alt=""/>
+      </a>
+      <div class="p-6">
+        <h5 class="text-gray-900 text-xl font-medium mb-2">Chatbot Application</h5>
+        <p class="text-gray-700 text-base mb-4">
+          It uses face detection and recognition solution to make camera surveillance system.
+        </p>
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
+      </div>
+    </div>
+    <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
+      <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+        <img class="rounded-t-lg" src="../assets/images/card13.jpg" alt=""/>
+      </a>
+      <div class="p-6">
+        <h5 class="text-gray-900 text-xl font-medium mb-2">Stable Diffusion Image Generation</h5>
+        <p class="text-gray-700 text-base mb-4">
+          It includes mobile and web application using face recognition to manage the employee and customers.
+        </p>
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
+      </div>
+    </div>
+    <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
+      <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+        <img class="rounded-t-lg" src="../assets/images/card14.jpg" alt=""/>
+      </a>
+      <div class="p-6">
+        <h5 class="text-gray-900 text-xl font-medium mb-2">Carbon Management System</h5>
+        <p class="text-gray-700 text-base mb-4">
+          Easiest way to start creating NFT Art using Artificial Intelligence. Start creating in minutes!
+        </p>
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
+      </div>
+    </div>
+    <div class="rounded-lg shadow-lg bg-white max-w-xs m-2">
+      <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+        <img class="rounded-t-lg" src="../assets/images/card15.jpg" alt=""/>
+      </a>
+      <div class="p-6">
+        <h5 class="text-gray-900 text-xl font-medium mb-2">Fruit and Vegetable Recognition</h5>
+        <p class="text-gray-700 text-base mb-4">
+          The idea was to build an application which recognizes the food item(s) from the captured photo and gives its user different recipes that can be made using the food item(s).
+        </p>
+        <a href="https://wa.me/+14422295661" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-green-700 hover:bg-green-400"><i id="social-whatsapp" class="fab fa-whatsapp text-xl social pr-2"></i>Demo</a>
       </div>
     </div>
   </div>
